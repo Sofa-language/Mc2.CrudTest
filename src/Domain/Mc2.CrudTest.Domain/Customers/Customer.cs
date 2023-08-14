bc322352653ac655c342d5ff8080197270846563
Aliakbar Jahanbin <jahanbin.ali1988@gmail.com>
2023-08-14 13:13:25 +0330

    create builder class for tests

--- src/Domain/Mc2.CrudTest.Domain/Customers/Customer.cs
+++ src/Domain/Mc2.CrudTest.Domain/Customers/Customer.cs
@@ -1,15 +1,30 @@
-﻿using Mc2.CrudTest.Presentation.Shared.SeedWork;
+﻿using Mc2.CrudTest.Domain.Customers.Initializers;
+using Mc2.CrudTest.Presentation.Shared.SeedWork;
 
 namespace Mc2.CrudTest.Domain.Customers
 {
     public class Customer : Entity<long>, IAggregateRoot
     {
-        public long Id { get; set; }
-        public string Firstname { get; set; }
-        public string Lastname { get; set; }
-        public DateTimeOffset DateOfBirth { get; set; }
-        public string PhoneNumber { get; set; }
-        public string Email { get; set; }
-        public string BankAccountNumber { get; set; }
+        private Customer() : base()
+        {
+            
+        }
+        public Customer(CreateOrUpdateInitializer initializer) : this()
+        {
+            Id = initializer.Id;
+            Firstname = initializer.Firstname;
+            Lastname = initializer.Lastname;
+            DateOfBirth = initializer.DateOfBirth;
+            PhoneNumber = initializer.PhoneNumber;
+            Email = initializer.Email;
+            BankAccountNumber = initializer.BankAccountNumber;
+        }
+        public long Id { get; private set; }
+        public string Firstname { get; private set; }
+        public string Lastname { get; private set; }
+        public DateTimeOffset DateOfBirth { get; private set; }
+        public string PhoneNumber { get; private set; }
+        public string Email { get; private set; }
+        public string BankAccountNumber { get; private set; }
     }
 }
