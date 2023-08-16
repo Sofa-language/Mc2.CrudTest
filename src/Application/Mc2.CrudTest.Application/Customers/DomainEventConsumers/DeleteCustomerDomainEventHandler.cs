ee20658182dab017106c05f1a7a3a8c8cfffd6c4
Aliakbar Jahanbin <jahanbin.ali1988@gmail.com>
2023-08-16 15:22:49 +0330

    add docker compose

--- src/Application/Mc2.CrudTest.Application/Customers/DomainEventConsumers/DeleteCustomerDomainEventHandler.cs
+++ src/Application/Mc2.CrudTest.Application/Customers/DomainEventConsumers/DeleteCustomerDomainEventHandler.cs
@@ -4,16 +4,16 @@ using Microsoft.Extensions.Logging;
 
 namespace Mc2.CrudTest.Application.Customers.DomainEventConsumers
 {
-    public class DeleteCustomerDomainEventHandler : DomainEventHandler<DeleteCustomerDomainEvent>
-    {
-        public DeleteCustomerDomainEventHandler(ILogger logger) : base(logger)
-        {
-        }
+    //public class DeleteCustomerDomainEventHandler : DomainEventHandler<DeleteCustomerDomainEvent>
+    //{
+    //    public DeleteCustomerDomainEventHandler(ILogger logger) : base(logger)
+    //    {
+    //    }
 
-        protected override async Task HandleEvent(DeleteCustomerDomainEvent notification, CancellationToken cancellationToken)
-        {
-            //Publish on a MessageBrocker on write on a secondary Database
-            await Task.FromResult(true);
-        }
-    }
+    //    protected override async Task HandleEvent(DeleteCustomerDomainEvent notification, CancellationToken cancellationToken)
+    //    {
+    //        //Publish on a MessageBrocker on write on a secondary Database
+    //        await Task.FromResult(true);
+    //    }
+    //}
 }
